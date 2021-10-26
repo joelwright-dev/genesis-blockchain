15e39e27fffc40767ca1150440f31ae3a3566921
Joel Wright <71912904+joelwright-dev@users.noreply.github.com>
2021-10-27 07:07:49 +1000

    Miner class implemented

--- wallet/index.js
+++ wallet/index.js
@@ -41,6 +41,12 @@ class Wallet {
 
         return transaction
     }
+
+    static blockchainWallet() {
+        const blockchainWallet = new this();
+        blockchainWallet.address = 'blockchain-wallet'
+        return blockchainWallet
+    }
 }
 
 module.exports = Wallet
